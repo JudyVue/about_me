bd541a86629b73aabbc6f45fd9483e0eb043aa75
JudyVue <judyvue87@gmail.com>
2016-07-20 18:15:06 -0700

    day 3 changes

--- app.js
+++ app.js
@@ -8,115 +8,177 @@ alert('Take a minute to read my story, then answer the questions accordingly. I 
 // global variable for if user answers yes or no
 var right = "Great job. You got it right!";
 var wrong = "Sorry, incorrect!";
+var score = 0;
 
 //the first prompt
-var firstPrompt = prompt ("Did Judy graduate with a degree in English? Type Yes or No.").toUpperCase();
+var firstPrompt = prompt ('Did Judy graduate with a degree in English? Type Yes or No.').toUpperCase()
 {
+
 //printing to the console
-  console.log('The user answered ' + firstPrompt + ' to this question.')
+  console.log('The user answered ' + firstPrompt + ' to this question.');
 
 //if statements prompting user to type yes or no
-//maybe put a while loop in here to force user to type 'yes' or 'no.'??
 
-    if (firstPrompt === "YES")
-    {
+
+    if (firstPrompt === "YES") {
        alert("You answered " + firstPrompt + ". "+ wrong );
+
     }
-    else if (firstPrompt === "NO")
-    {
+    else if (firstPrompt === "NO") {
        alert("You answered " + firstPrompt + ". "+right);
+       score++;
     }
-    else
-    {
+    else {
       alert('WTF, dude! I said type yes or no!');
     }
-
 };
+
 //second prompt
-var secondPrompt = prompt ("Did Judy graduate in 2007? Type Yes or No.").toUpperCase();
+var secondPrompt = prompt ("Did Judy graduate in 2007? Type Yes or No.").toUpperCase()
 {
 
   //printing to the console
   console.log('The user answered ' + secondPrompt + ' to this question.')
 
 //if statements prompting user to type yes or no
-  if (secondPrompt === "YES")
-    {
+  if (secondPrompt === "YES") {
        alert("You answered " + secondPrompt + ". "+ wrong );
+
     }
-    else if (secondPrompt === "NO")
-    {
+    else if (secondPrompt === "NO") {
        alert("You answered " + secondPrompt + ". "+right);
+       score++;
     }
-    else
-    {
+    else {
       alert('WTF, dude! I said type yes or no!');
     }
 };
 
 //third prompt
-var thirdPrompt = prompt ("Did Judy teach English in North Korea? Type Yes or No.").toUpperCase();
+var thirdPrompt = prompt ("Did Judy teach English in North Korea? Type Yes or No.").toUpperCase()
 {
 
   //printing to the console
   console.log('The user answered ' + thirdPrompt + ' to this question.')
 
 //if statements prompting user to type yes or no
-  if (thirdPrompt === "YES")
-    {
+  if (thirdPrompt === "YES") {
        alert("You answered " + thirdPrompt + ". "+ wrong );
+
     }
-    else if (thirdPrompt === "NO")
-    {
+    else if (thirdPrompt === "NO") {
        alert("You answered " + thirdPrompt + ". "+right);
+       score++;
     }
-    else
-    {
+    else {
       alert('WTF, dude! I said type yes or no!');
     }
 };
 
 //fourth prompt
-var fourthPrompt = prompt ("Was Judy formerly a paralegal? Type Yes or No.").toUpperCase();
+var fourthPrompt = prompt ("Was Judy formerly a paralegal? Type Yes or No.").toUpperCase()
 {
 
   //printing to the console
   console.log('The user answered ' + fourthPrompt + ' to this question.')
 
 //if statements prompting user to type yes or no
-  if (fourthPrompt === "NO")
-    {
+  if (fourthPrompt === "NO"){
        alert("You answered " + fourthPrompt + ". "+wrong);
-    }
-    else if (fourthPrompt == "YES")
-    {
+    } else if (fourthPrompt == "YES") {
        alert("You answered " + fourthPrompt + ". "+ right );
-    }
-    else
-    {
+       score++;
+    } else  {
       alert('WTF, dude! I said type yes or no!');
     }
 };
 
 //fifth prompt
-var fifthPrompt = prompt ("Does Judy perform improv as a hobby? Type Yes or No.").toUpperCase();
+var fifthPrompt = prompt ("Does Judy perform improv as a hobby? Type Yes or No.").toUpperCase()
 {
   //printing to the console
   console.log('The user answered ' + fifthPrompt + ' to this question.')
 
 //if statements prompting user to type yes or no
-  if (fifthPrompt === "NO")
-  {
+  if (fifthPrompt === "NO"){
      alert("You answered " + fifthPrompt + ". "+wrong);
-  }
-  else if(fifthPrompt === "YES")
-  {
+  } else if(fifthPrompt === "YES"){
      alert("You answered " + fifthPrompt + ". "+ right );
-  }
-  else
-  {
+     score++;
+  } else {
     alert('WTF, dude! I said type yes or no!');
   }
 };
+
+//sixth prompt
+
+//for loop
+for (var i = 0; i < 4; i++){
+
+//declare the variables and prompt
+  var sixthPrompt = prompt ('BONUS QUESTION: How many nieces/nephews (combined) does Judy have?');
+  sixthPrompt = parseInt(sixthPrompt);
+
+//console statements for testing
+  console.log('The answer is ' + sixthPrompt);
+  console.log(typeof sixthPrompt);
+//my if statements
+    if (sixthPrompt < 6) {
+    alert ('Too low! Go higher!');
+    continue;
+  } else if (sixthPrompt > 6) {
+    alert ('Too high! Go lower!');
+    continue;
+  } else if (isNaN(sixthPrompt) === true) { //checks to see if user entered string
+    alert ('Type a number!');
+    continue;
+  } else {
+    alert ('That is right! Judy has 6 nieces and nephews total!');
+    score++;
+    break;
+  }
+
+};
+
+//seventh prompt
+var countries;
+countries = ['South Korea', 'China', 'Japan', 'Mongolia', 'Thailand', 'Laos'];
+
+//for loops
+var numberOfGuesses = 0;
+
+
+while (numberOfGuesses < 6){
+  //declare the variables and prompt
+  var seventhPrompt = prompt('BONUS QUESTION: Judy has visited several countries in Asia. You have 6 chances to guess one Asian country she has been to. Go!').toUpperCase();
+
+  //boolean
+  var finish = false;
+
+  for (var k = 0; k < countries.length; k++) {
+  if (seventhPrompt === countries[k].toUpperCase()) {
+      alert ('Congrats! Judy has visited ' + countries[k] + '! In total, Judy has visited ' + countries + '.');
+      numberOfGuesses = 7;
+      score++;
+      finish = true;
+      break;
+  }
+}
+  if (finish === false) {
+    alert ('Nope! Keep guessing!');
+    numberOfGuesses += 1;
+  }
+
+};
+
+//scorekeeping
+if (score >= 4){
+  alert ('Your score was ' + score + ' out of 7. Congrats!');
+}
+else {
+  alert ('Your score was ' + score + ' out of 7. We\'re not going on a second date!');
+}
+
+
 
 };
